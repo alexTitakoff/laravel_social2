d4d63ab96db15d13e493eeb3abd4c769a3f0ab5b
alex <alex.iTakov@yandex.ru>
2016-09-04 15:57:37 +0300

    sign up controller and model // signup good

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 
-class UserControllers extends Controller
+class UserController extends Controller
 {
 	public function postSignUp(Request $request) {
 
